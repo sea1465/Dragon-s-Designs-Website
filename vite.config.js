e1d6b3c123ccc089ea02f545451ae2dfbdd4591b
sea1465 <khushvadera@gmail.com>
2025-11-17 12:26:27 +0000

    Set vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Dragon-s-Designs-Website/',
   plugins: [react()],
   server: { port: 3000 },
+  preview: {
+    port: 5173
+  }
 });
+
